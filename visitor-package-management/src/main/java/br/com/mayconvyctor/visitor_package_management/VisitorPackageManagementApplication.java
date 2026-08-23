f19bbb7d2188e680ca3e5f19fd4c3b0af5d10605
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-08-22 22:30:01 -0300

    chore: initial commit do spring boot vazio

---
+++ visitor-package-management/src/main/java/br/com/mayconvyctor/visitor_package_management/VisitorPackageManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.mayconvyctor.visitor_package_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorPackageManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorPackageManagementApplication.class, args);
+	}
+
+}
---
+++ visitor-package-management/src/test/java/br/com/mayconvyctor/visitor_package_management/VisitorPackageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mayconvyctor.visitor_package_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorPackageManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
