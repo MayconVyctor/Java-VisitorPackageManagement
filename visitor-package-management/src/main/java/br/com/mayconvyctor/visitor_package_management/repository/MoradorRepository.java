e8071af68595483e3106244d547a2a5caa583013
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-08-25 18:06:47 -0300

    refactor: retira comentarios do repository e passa eles para o obsidian, para deixar o codigo mais limpo

--- visitor-package-management/src/main/java/br/com/mayconvyctor/visitor_package_management/repository/MoradorRepository.java
+++ visitor-package-management/src/main/java/br/com/mayconvyctor/visitor_package_management/repository/MoradorRepository.java
@@ -6,32 +6,8 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-/**
- * @Repository: Diz ao Spring que esta interface é um componente focado em
- * acesso a dados (Data Access Object - DAO). O Spring vai gerenciar isso na memória.
- */
 @Repository
 public interface MoradorRepository extends JpaRepository<Morador, Long> {
-
-    /**
-     * EXTENDENDO O JpaRepository:
-     * Ao herdar de JpaRepository<Morador, Long>, nós ganhamos de graça (sem escrever uma
-     * linha de código) métodos como: save(), findAll(), findById(), deleteById().
-     * O 'Morador' é a tabela alvo, e o 'Long' é o tipo da Chave Primária (ID).
-     */
-
-    /**
-     * QUERY METHODS (A mágica do Spring Data):
-     * Nós podemos criar buscas customizadas apenas escrevendo o nome do método no padrão correto.
-     * O Spring lê "findByCpf" e gera automaticamente um: "SELECT * FROM TB_MORADORES WHERE cpf = ?"
-     *
-     * Retornamos um Optional<> pois é a melhor prática (Clean Code) para buscas
-     * que podem não encontrar resultados. Isso evita o temido NullPointerException.
-     */
     Optional<Morador> findByCpf(String cpf);
-
-    /**
-     * Retorna uma lista de moradores pelo número do apartamento.
-     */
     Iterable<Morador> findByApartamento(String apartamento);
 }
