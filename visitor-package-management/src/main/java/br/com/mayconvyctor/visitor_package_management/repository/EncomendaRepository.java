8afd4bcbe9d13d587589115ebd333bcfd58cc1b7
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-08-27 23:07:30 -0300

    feat(model): cria repositorio de Encomenda com relacionamento ManyToOne

--- visitor-package-management/src/main/java/br/com/mayconvyctor/visitor_package_management/repository/EncomendaRepository.java
+++ visitor-package-management/src/main/java/br/com/mayconvyctor/visitor_package_management/repository/EncomendaRepository.java
@@ -1,4 +1,13 @@
 package br.com.mayconvyctor.visitor_package_management.repository;
 
-public interface EncomendaRepository {
+import br.com.mayconvyctor.visitor_package_management.model.Encomenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EncomendaRepository extends JpaRepository<Encomenda, Long> {
+
+    // Query Method customizado: O Spring gera um SELECT para buscar
+    // todas as encomendas baseadas no ID de um morador específico.
+    Iterable<Encomenda> findByMoradorId(Long moradorId);
 }
