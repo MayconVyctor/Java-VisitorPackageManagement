dafe42721972ff58b97673635f35e2d12662e823
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-09-02 00:46:16 -0300

    feat(service): migra os comentarios do codigo para a documentacao no obsidian

--- visitor-package-management/src/main/java/br/com/mayconvyctor/visitor_package_management/service/EncomendaService.java
+++ visitor-package-management/src/main/java/br/com/mayconvyctor/visitor_package_management/service/EncomendaService.java
@@ -14,11 +14,6 @@ public class EncomendaService {
     private final EncomendaRepository encomendaRepository;
     private final NotificacaoProducer notificacaoProducer;
 
-    /**
-     * Injeção de dependências via construtor.
-     * Repare que o Service conversa com o Repository e com o Producer,
-     * mantendo o isolamento (Clean Architecture).
-     */
     public EncomendaService(EncomendaRepository encomendaRepository, NotificacaoProducer notificacaoProducer) {
         this.encomendaRepository = encomendaRepository;
         this.notificacaoProducer = notificacaoProducer;
@@ -31,18 +26,13 @@ public class EncomendaService {
 
     @Transactional
     public Encomenda registrar(Encomenda encomenda) {
-        // 1. Salva a encomenda no banco de dados Oracle
         Encomenda encomendaSalva = encomendaRepository.save(encomenda);
-
-        // 2. Monta a mensagem que será enviada para o mensageiro
         String mensagem = String.format(
                 "NOVA ENCOMENDA: '%s' recebida na portaria para o Morador %s (Apto: %s).",
                 encomendaSalva.getDescricao(),
                 encomendaSalva.getMorador().getNomeCompleto(),
                 encomendaSalva.getMorador().getApartamento()
         );
-
-        // 3. Dispara a notificação assíncrona para o Kafka
         notificacaoProducer.enviarNotificacao(mensagem);
 
         return encomendaSalva;
