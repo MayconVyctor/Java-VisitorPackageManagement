415f0c2dab37d713fc1272074ff61390e4eba0a7
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-09-03 23:54:40 -0300

    feat(kafka): migra a documentacao de NotificacaoConsumer para o obsidian

--- visitor-package-management/src/main/java/br/com/mayconvyctor/visitor_package_management/consumer/NotificacaoConsumer.java
+++ visitor-package-management/src/main/java/br/com/mayconvyctor/visitor_package_management/consumer/NotificacaoConsumer.java
@@ -5,18 +5,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class NotificacaoConsumer {
-
-    /**
-     * @KafkaListener: Essa anotação é o coração do consumidor.
-     * Ela diz ao Spring: "Fique escutando o tópico 'encomendas-topic'.
-     * Toda vez que chegar uma mensagem lá, execute este método imediatamente."
-     */
     @KafkaListener(topics = "encomendas-topic", groupId = "condo-group")
     public void escutarNotificacao(String mensagem) {
-        // Aqui simularíamos o envio de um e-mail usando JavaMailSender ou uma API de SMS (Twilio)
-        System.out.println("=========================================================");
+        System.out.println("");
         System.out.println("[CONSUMER] Processando notificação em segundo plano: ");
         System.out.println( mensagem);
-        System.out.println("=========================================================");
+        System.out.println("");
     }
 }
